89efa215582b13b80e226a1ddbb1d67f23429c35
hoangnm-ndm <hoangnm.ndm@gmail.com>
2025-06-16 17:20:51 +0700

    add cors

--- index.js
+++ index.js
@@ -4,12 +4,20 @@ import { PORT, HOST } from "./src/common/configs/environments.js";
 import router from "./src/routes/index.js";
 import errorHandler from "./src/common/middlewares/errorHandle.js";
 import setupSwagger from "./src/common/configs/swagger-config.js";
+import cors from "cors";
 
 connectDB();
 
 const app = express();
 
 app.use(express.json());
+
+app.use(
+	cors({
+		origin: "http://localhost:5173", // chỉ cho phép domain này gọi API
+		credentials: true, // cho phép gửi cookie
+	})
+);
 
 app.use("/api", router);
 
